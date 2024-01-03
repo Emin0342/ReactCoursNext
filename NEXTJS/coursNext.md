e91f514c2d91d7e13b336a7a79cf2abb90449e85
CORUMLU Emin <emin.corumlu03@gmail.com>
2024-01-03 14:26:04 +0100

    Setup projet Next

---
+++ NEXTJS/coursNext.md
@@ -0,0 +1,33 @@
+# Next JS
+
+NextJS est un frameword React qui permet de creer des applications web. L'interface se compose de composants React et le framework se charge d'apporter tous les outils de bundling, compilation et bien plus...
+
+## Principales fonctionnalités
+
+- Système de routage : NextJS utilise un système de routage directement basé sur le système de fichiers.
+- Rendu performant : NextJS permet de faire du rendu server (Server Side Rendering) et du rendu statique (Static Site Generation).
+- Data Fetching : NextJS facilite l'utilisation d'async await pour faire des appels API.
+- CSS : NextJS permet d'utiliser CSS, Saas, Less, Stylus, CSS Modules, Styled Components et bien plus...
+- TypeScript : Next.JS permet d'utiliser TypeScript.
+
+## Installation
+
+- Pre requis : NodeJS 18.17 ou plus 
+- Compte GitHub 
+- Compte vercel
+
+## Création d'un projet
+
+Pour créer un projet NextJS, il suffit d'executer la commande suivante :
+
+```bash
+npx create-next-app
+```
+Ensuite repondre : 
+
+Le nom de l'app
+TypeScript : Oui
+ESLint : Oui
+Tailwind CSS : Oui
+src /directory : Oui
+import alias : (très utile aparamant mais ici on repond non)
--- codeprof2/useReducer/code/13-finished/src/store/auth-context.js
+++ codeprof2/useReducer/code/13-finished/src/store/auth-context.js
@@ -1,38 +1,48 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 const AuthContext = React.createContext({
-  isLoggedIn: false,
-  onLogout: () => {},
-  onLogin: (email, password) => {}
+  state : {isLoggedIn: false},
+  dispatch : (action) => {},
 });
 
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return {isLoggedIn: true};
+    case 'LOGOUT':
+      return {isLoggedIn: false};
+    default:
+      return state;
+  }
+};
+
 export const AuthContextProvider = (props) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+ // const [isLoggedIn, setIsLoggedIn] = useState(false);
+ const [state, dispatch] = useReducer(authReducer, {isLoggedIn: false});
 
   useEffect(() => {
     const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
 
     if (storedUserLoggedInInformation === '1') {
-      setIsLoggedIn(true);
+      dispatch({type: 'LOGIN'});
     }
   }, []);
 
-  const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn');
-    setIsLoggedIn(false);
-  };
+  // const logoutHandler = () => {
+  //   localStorage.removeItem('isLoggedIn');
+  //   setIsLoggedIn(false);
+  // };
 
-  const loginHandler = () => {
-    localStorage.setItem('isLoggedIn', '1');
-    setIsLoggedIn(true);
-  };
+  // const loginHandler = () => {
+  //   localStorage.setItem('isLoggedIn', '1');
+  //   setIsLoggedIn(true);
+  // };
 
   return (
     <AuthContext.Provider
       value={{
-        isLoggedIn: isLoggedIn,
-        onLogout: logoutHandler,
-        onLogin: loginHandler,
+        state,
+        dispatch
       }}
     >
       {props.children}
